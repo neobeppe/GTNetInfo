291c615062b9c6a6e9244e0a1163461c301cec89
neobeppe <giuseppe.travasoni@gmail.com>
2015-12-05 16:39:44 +0100

    fix example

--- Example/GTNetInfo/GTViewController.m
+++ Example/GTNetInfo/GTViewController.m
@@ -29,8 +29,8 @@
 -(void)updateUIWithNetInfo:(GTNetInfo *)netInfo {
     [netInfo isConnected] ? [self.connectedLabel  setText:@"Connected: YES"] : [self.connectedLabel  setText:@"Connected: NO"];
     [netInfo isWifi] ? [self.wifiConnectedLabel  setText:@"WiFi Connection: YES"] : [self.wifiConnectedLabel  setText:@"WiFi Connection: NO"];
-    netInfo.currentSSID ? [self.wifiSsidLabel setText:[NSString stringWithFormat:@"WiFi SSID: %@",netInfo.currentSSID]] : [self.wifiSsidLabel setText:@""];
-    netInfo.currentRadioTecnology ? [self.cellularTechnologyLabel setText:[NSString stringWithFormat:@"Cellular: %@",netInfo.currentRadioTecnology]] : [self.cellularTechnologyLabel setText:@""];
+    netInfo.SSID ? [self.wifiSsidLabel setText:[NSString stringWithFormat:@"WiFi SSID: %@",netInfo.SSID]] : [self.wifiSsidLabel setText:@""];
+    netInfo.radioTecnology ? [self.cellularTechnologyLabel setText:[NSString stringWithFormat:@"Cellular: %@",netInfo.radioTecnology]] : [self.cellularTechnologyLabel setText:@""];
 }
 
 - (IBAction)refreshButtonTapped:(id)sender {
